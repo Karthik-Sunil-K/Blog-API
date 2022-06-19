3601dafb4f5369ba80cf87816953641b1c171f7a
Karthik-Sunil-K <karthiksunil038@gmail.com>
2022-06-19 13:41:01 +0530

    password encrypted

--- routes/auth.js
+++ routes/auth.js
@@ -2,14 +2,18 @@ const express = require('express');
 const { json } = require('express/lib/response');
 const router = express.Router();
 const User = require('../model/users')
+const bcrypt = require('bcrypt');
 
 
 router.post('/register', async (req, res) => {
     try {
+        const salt = await bcrypt.genSalt(10);
+        const rawPassword = req.body.password
+        const hashedPassword= await bcrypt.hash(rawPassword,salt)
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password
+            password: hashedPassword
         })
 
         const user = await newUser.save();
