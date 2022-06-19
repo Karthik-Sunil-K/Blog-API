61a1dac4199ace0b359dc5db76a8f599f14da6aa
Karthik-Sunil-K <karthiksunil038@gmail.com>
2022-06-19 18:22:25 +0530

    login route added remove pass

--- routes/auth.js
+++ routes/auth.js
@@ -40,7 +40,8 @@ router.post('/login', async(req,res)=>{
         if(!validated){
             res.status(400).json({message:"wrong credentials"})
         }
-        res.status(200).json(user)
+        const {password, ...others}=user._doc
+        res.status(200).json(others)
     } catch (err) {
         res.status(500).json(err)
     }
