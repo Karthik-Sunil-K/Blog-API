ac40bcc3965950e63cf6637ee0313682a7a267d3
Karthik-Sunil-K <karthiksunil038@gmail.com>
2022-06-19 18:17:36 +0530

    login route added

--- routes/auth.js
+++ routes/auth.js
@@ -27,4 +27,25 @@ router.post('/register', async (req, res) => {
         })
     }
 })
+
+//login
+
+router.post('/login', async(req,res)=>{
+    try {
+        const user = await User.findOne({username:req.body.username})
+        if(!user){
+            res.status(400).json({message:"wrong credentials"})
+        }
+        const validated = await bcrypt.compare(req.body.password,user.password)
+        if(!validated){
+            res.status(400).json({message:"wrong credentials"})
+        }
+        res.status(200).json(user)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+
+})
+
+
 module.exports = router
