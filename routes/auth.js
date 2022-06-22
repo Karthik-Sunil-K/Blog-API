66c13019bd7a9d1a36320b00f97f2d49a1271403
Karthik-Sunil-K <karthiksunil038@gmail.com>
2022-06-22 19:35:36 +0530

    username exist check

--- routes/auth.js
+++ routes/auth.js
@@ -9,41 +9,48 @@ router.post('/register', async (req, res) => {
     try {
         const salt = await bcrypt.genSalt(10);
         const rawPassword = req.body.password
-        const hashedPassword= await bcrypt.hash(rawPassword,salt)
-        const newUser = new User({
-            username: req.body.username,
-            email: req.body.email,
-            password: hashedPassword
-        })
-        
-        const user = await newUser.save();
+        const hashedPassword = await bcrypt.hash(rawPassword, salt)
+        const exitUsername = await User.findOne({ username: req.body.username });
+        if (!exitUsername) {
+            const newUser = new User({
+                username: req.body.username,
+                email: req.body.email,
+                password: hashedPassword
+            })
+            const user = await newUser.save();
+            res.status(200).json({
+                message: "user created succesfully",
+                user: user
+            })
+        }
+        console.log("user name already exist")
         res.status(200).json({
-            message :"user created succesfully",
-            user:user
-        })
+            message: "username already exists"
+        });
+
     } catch (err) {
         res.status(500).json({
-            message:err
+            message: err
         })
     }
 })
 
 //login
 
-router.post('/login', async(req,res)=>{
+router.post('/login', async (req, res) => {
     try {
-        const user = await User.findOne({username:req.body.username})
-        if(!user){
-            res.status(400).json({message:"wrong credentials"})
+        const user = await User.findOne({ username: req.body.username })
+        if (!user) {
+            res.status(400).json({ message: "wrong credentials" })
         }
-        const validated = await bcrypt.compare(req.body.password,user.password)
-        if(!validated){
-            res.status(400).json({message:"wrong credentials"})
+        const validated = await bcrypt.compare(req.body.password, user.password)
+        if (!validated) {
+            res.status(400).json({ message: "wrong credentials" })
         }
-        const {password, ...others}=user._doc
+        const { password, ...others } = user._doc
         res.status(200).json({
-            message:"logined successfully",
-            user:others
+            message: "logined successfully",
+            user: others
         })
     } catch (err) {
         res.status(500).json(err)
