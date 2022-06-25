0a8b8c1efcc225b31a761d801b6d652a45339123
Karthik-Sunil-K <karthiksunil038@gmail.com>
2022-06-25 17:33:24 +0530

    post post and update route added

--- routes/post.js
+++ routes/post.js
@@ -2,27 +2,51 @@ const express = require('express');
 const { json, send } = require('express/lib/response');
 const router = express.Router();
 const User = require('../model/users')
-const Post =require('../model/post')
+const Post = require('../model/post')
 const { db } = require('../model/users');
+const { route } = require('./auth');
 
 
 
-router.post('/', async (req,res)=>{
-    
- try {
-    const users= await User.find()
-    res.status(200).json({
-        count:users.length,
-       message:user
-    })
- } catch (error) {
-    res.status(500).json({
-        
-        message:"cant fetch users details"+error
-    })
- }    
+router.post('/newpost', async (req,res)=>{
+    const newpost = new Post(req.body);
+    try {
+        const post = await newpost.save();
+        res.status(200).json(post)
+    } catch (error) {
+        res.status(500).json({
+            message:"cant add new post "+error
+        })
+    }
+});
+
+router.put('/update/:id',async (req,res)=>{
+    const postId=req.params.id;
+    try {
+        const post = await Post.findById(postId)
+        if(post.username===req.body.username){
+            try {
+               const updatedPost= await Post.findByIdAndUpdate(postId,{$set:req.body},{new:true});
+               res.status(200).json({
+                message:"post updated succesfully",
+                updatedPost:updatedPost
+               })
+            } catch (err) {
+                res.status(500).json({
+                    message:"cant update post"
+                })
+            }
+        }else{
+            res.status(402).json({
+                message:"u can only update ur post"
+            })
+        }
+    } catch (err) {
+        res.status(500).json({
+            message:"cant fetch post "+err
+        })
+    }
 })
-
 
 
 module.exports = router
